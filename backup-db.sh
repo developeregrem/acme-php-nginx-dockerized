330cce364675cc1594dd67b5102343fe479fd17a
Alexander Elchlepp <alexander@elchlepp.at>
2019-06-16 14:42:06 +0200

    changed working directory

--- backup-db.sh
+++ backup-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd /opt/acme-php-nginx-dockerized
+cd "$(dirname "$0")"
 /usr/local/bin/docker-compose exec -T db /bin/sh -c "chmod 0744 /db/backup_mysql_cron.sh && /db/backup_mysql_cron.sh"
--- update-docker.sh
+++ update-docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /opt/acme-php-nginx-dockerized
-/usr/local/bin/docker-compose pull
+cd "$(dirname "$0")"
 
+/usr/local/bin/docker-compose pull
 /usr/local/bin/docker-compose build --force-rm --pull
 /usr/local/bin/docker-compose stop
 /usr/local/bin/docker-compose up --force-recreate -d
